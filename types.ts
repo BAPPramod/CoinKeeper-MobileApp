da38967ab3dbb8ef1d8e6e87a2aaf181989271c4
Pramod <bappramod@gmail.com>
2025-04-10 20:11:12 +0300

    Welcome Page

---
+++ types.ts
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import { TextProps, TextStyle, TouchableOpacityProps, ViewStyle, } from "react-native";
+
+export type ScreenWrapperProps = {
+  style?: ViewStyle;
+  children: React.ReactNode;
+};
+
+export type TypoProps = {
+  size?: number;
+  color?: string;
+  fontWeight?: TextStyle["fontWeight"];
+  children: any | null;
+  style?: TextStyle;
+  textProps?: TextProps;
+};
+
+export interface CustomButtonProps extends TouchableOpacityProps {
+  style?: ViewStyle;
+  onPress?: () => void;
+  loading?: boolean;
+  children: React.ReactNode;
+}
